193d173a71cad0c8b1dac45467a85be8d0949ffc
tuhnik <huntlarsen@gmail.com>
2018-09-26 21:14:51 +0300

    ...

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,7 +3,12 @@
     <input type = "text" class = "input" placeholder = "What next?"
     v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="(item, i) in todos" :key = "item.id" class = "item">
-      <div>{{item.id}}. {{item.title}}</div>
+      <div class = "item-left" @dblclick="editTodo(item)">
+      <div v-if="!item.editing" class="item-label">{{item.title}}</div>
+      <input v-else class= "item-edit" v-model="item.title" @blur="doneEditing(item)"
+      @keyup.enter="doneEditing(item)" type = "text"
+      v-focus>
+    </div>
       <div class = "close" @click="removeTodo(i)">&#10006;</div>
     </div>
   </div>
@@ -16,10 +21,21 @@ export default {
     return {
       todoId: 3,
       newTodo: '',
-      todos: [{ id: 1, title: 'Mine poodi', completed: false },
-        { id: 2, title: 'Osta kartuleid', completed: false },
+      todos: [{
+        id: 1, title: 'Mine poodi', completed: false, editing: false,
+      },
+      {
+        id: 2, title: 'Osta kartuleid', completed: false, editing: false,
+      },
       ],
     };
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus();
+      },
+    },
   },
   methods: {
     addTodo() {
@@ -30,12 +46,19 @@ export default {
         id: this.todoId,
         title: this.newTodo,
         completed: false,
+        editing: false,
       });
       this.newTodo = '';
       this.todoId += 1;
     },
     removeTodo(i) {
       this.todos.splice(i, 1);
+    },
+    editTodo(item) {
+      item.editing = true; // eslint-disable-line no-param-reassign
+    },
+    doneEditing(item) {
+      item.editing = false; // eslint-disable-line no-param-reassign
     },
   },
 };
@@ -59,6 +82,7 @@ export default {
 }
 
 .item {
+  width: 100%;
   margin-bottom: 12px;
   display: flex;
   align-items: center;
@@ -66,6 +90,17 @@ export default {
   background: rgb(236, 236, 236);
   padding: 10px 18px;
   border-radius: 5px;
+}
+.item-left {
+  width: 100%;
+}
+.item-edit {
+  width: 100%;
+  font-size: 16px;
+  background: rgb(236, 236, 236);
+  outline: none;
+  border: none;
+
 }
 
 .close {
